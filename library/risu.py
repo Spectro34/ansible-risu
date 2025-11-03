b67c9e90991f8ecf1502cc34919b2df78ee6a3df
Harshvardhan Sharma <37374433+Spectro34@users.noreply.github.com>
2025-11-03 18:25:24 +0530

    Update risu.py

--- library/risu.py
+++ library/risu.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 # Copyright: (c) 2024, Harshvardhan Sharma
-# GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
 
